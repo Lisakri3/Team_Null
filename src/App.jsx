728c04e9d9a4f0214e014dc2870cdb07659573fe
Lisakri3 <lisa.krings3@gmail.com>
2024-02-15 21:14:11 +0100

    Try and Error

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from './components/Header';
+
+function App() {
+ return (
+    <div>
+        <header>
+            <p>Die Taverne</p>
+        </header>
+        <main>
+
+
+        </main>
+    </div>
+ );
+}
+
+export default App;
